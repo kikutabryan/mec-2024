55501c5da56aff97a3fff69f999818f8f112d643
Bryan Kikuta <112445285+kikutabryan@users.noreply.github.com>
2024-11-16 07:02:26 -0500

    setup env

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup()
+{
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
+
+/**
+ * @brief Adds two integers together.
+ *
+ * This function takes two integer parameters, x and y, and returns their sum.
+ */
+int myFunction(int x, int y)
+{
+  return x + y;
+}
